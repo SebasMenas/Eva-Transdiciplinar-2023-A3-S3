d58396bb1895a64234dba039c0c050cf06fc8b03
Delta <119447307+T0kyoB0y@users.noreply.github.com>
2023-05-25 09:46:06 -0400

    Creación del main.pu
    Archivo que crea una interfaz usando customtkinter, y añade textbuttons, buttonframes que permiten introducir texto/numeros y permiten elegir que medidas de distancia/tiempo usan.

---
+++ Interfaz/main.py
@@ -0,0 +1,95 @@
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣶⠿⠟⠋⠙⠻⠿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⡿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢻⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⠿⠛⠁⠀⠀⠀⠀⡶⢤⣄⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⣿⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀⢀⠤⠤⣝⡻⢷⣾⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠈⠍⠶⢖⢲⣝⣿⡇⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣶⣿⠿⠀⣴⡶⠦⠀⣜⣺⠿⠇⠸⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢰⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⢾⠟⠉⠀⠀⢀⣥⡰⣶⡻⣮⣟⠟⠁⠀⠈⠙⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣴⡀⠀⠀⠀⠀⣠⣚⣳⣯⡻⠯⠟⠃⠁⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀ ⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⣸⣿⠟⠃⠀⠀⠀⣻⣿⠷⣿⡝⠀⠀⠀⠀⠀⢀⣤⣖⣶⣶⣶⣾⣿⣧⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⢠⣿⠏⠀⠀⠀⠀⠀⣛⣿⡽⣩⣧⠀⠀⠀⢀⢾⣟⡷⠛⠉⠁⠀⠀⠙⢿⡆⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⢸⠋⠀⠀⠀⠀⠀⠀⠈⠫⣾⣿⣿⡷⣴⢤⣤⢠⣤⣄⠀⠀⠀⠀⠀⠀⠀⠙⠀⠀⠀⠀ ⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣲⣼⣟⡿⣷⡈⠛⠿⢾⣫⣾⣟⣿⣟⣾⣗⣄⠀⠀⠐⠲⣤⣄⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢨⣿⡇⠀⠀⠀⠀⠀⠀⠘⣶⣶⡿⠋⠁⠀⠈⠻⣷⣿⣦⡀⠀⠀⠀⠀⠀⠈⠽⣿⣯⡇⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⠀⠀⠀⠈⠻⡿⣫⣷⣤⣄⣤⣤⠀⣸⢽⣿⡆⠀⠀⠀⢸⡟⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⠀⠀⢰⣀⠀⠀⠀⠈⠉⠑⠛⠛⢋⡠⡼⣝⡿⠶⠃⠀⠀⠀⡾⠁⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠸⣇⠀⡀⣤⣦⣶⣮⢿⣼⣿⣿⣧⠻⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⡸⣿⣾⣛⡷⠽⠗⠛⠋⠉⠀⠀⣀⣠⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⢿⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣻⠿⣖⠟⠁⠀⢀⣀⣤⣤⣤⣀⠀⠈⢿⠃⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⡿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⣷⣤⡀⠀⠀⠀⠀⠀⠀⢿⣿⣲⠀⠀⠀⣚⠋⠀⠀⠈⣹⡂⠀⠈⠀⠀⠀⠀⠀⢀⣠⣶⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣶⣄⡀⠀⠀⠀⠙⠽⣾⢧⣶⢤⢤⣲⣖⣆⠯⠛⠀⠀⠀⠀⠀⢀⣠⣶⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⣿⣦⣄⠀⠀⠀⠉⠉⠛⠛⠛⠙⠉⠁⠀⠀⠀⠀⠀⢠⣶⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⡟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣷⣄⡀⠀⠀⠀⠀⢀⣤⣾⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⣿⣦⣶⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+import matplotlib.pyplot as plt
+#import pygame as py
+import pymunk as mk
+import numpy as np
+import pandas as pnd
+import matplotlib
+import customtkinter as ctk
+
+#?                          Setup principal de la main_window
+
+main_window = ctk.CTk()                              #   Asignamos las propiedades de ctk.Ctk a main_window
+main_window.title("Proyecto")                        #   Ponemos nombre a la ventanita creada
+main_window.geometry("1000x600")                     #   Definimos la resolucion de la ventana
+main_window_width, main_window_height = main_window.winfo_geometry()[0], main_window.winfo_geometry()[1] #   Obtenemos el alto y ancho
+main_window.resizable(False, False)                  #   Denegamos el ajuste de ventana en el eje x e y
+#?                           #####################################
+
+
+#* Ver opción seleccionada
+def selected(choice):
+    print(choice)
+
+graphic_frame = ctk.CTkFrame(main_window, width=740, height=560)    #Frame para el gráfico  "graphic_frame"
+graphic_frame.grid(row=0, column=1, pady=20)                        #Posicionamiento del frame para gráficos
+
+buttons_frame = ctk.CTkFrame(main_window, width=200, height=560)    #Frame para Botones/textboxes   "buttons_frame"
+buttons_frame.grid(row=0, column=0, padx=20, pady=20)               #Posicionamiento del frame para los botones/texboxes
+#*                                                                                                                                   #Todos estos botones están dentro de buttons_frame
+#Ocupan filas 0;1                                                                                                                    #Lo mismo para cada botón con estas mismas caracteristicas
+txt_1_label = ctk.CTkLabel(buttons_frame, width=5, height=10, text="Posición Inicial:", font=ctk.CTkFont(size=12, weight="bold"))    #Crear texto dentro de buttons_frame
+txt_1_label.grid(row=0, column=0, columnspan=2, pady=15, sticky="sew")                                                               #Posicionamiento del texto dentro de buttons_frame
+txt_1 = ctk.CTkTextbox(buttons_frame, width=60, height=8, activate_scrollbars=False)                                                 #Textbox para almacenar valores
+txt_1.grid(row=1, column=0, padx=2, pady=5, sticky="ne")                                                                             #Posicion de la textbox
+b_1 = ctk.CTkSegmentedButton(buttons_frame,width=60, height=10 , values=[" Km ", "  m  "], command=selected)                         #Segmented button, para 2 botones en 1
+b_1.grid(row=1, column=1, padx=2, pady=8, sticky="nw")                                                                               #Posicion y config del segmented button
+b_1.set("  m  ")
+
+#Ocupan filas 3;4
+txt_2_label = ctk.CTkLabel(buttons_frame, width=5, height=10, text="Velocidad inicial:", font=ctk.CTkFont(size=12, weight="bold"))
+txt_2_label.grid(row=3, column=0, columnspan=2, pady=15, sticky="sew")
+txt_2 = ctk.CTkTextbox(buttons_frame, width=60, height=8, activate_scrollbars=False)
+txt_2.grid(row=4, column=0, padx=2, pady=5, sticky="ne")
+b_2 = ctk.CTkSegmentedButton(buttons_frame,width=60, height=10 , values=["Km/h", " m/s "], command=selected)
+b_2.grid(row=4, column=1, padx=2, pady=8, sticky="nw")
+b_2.set(" m/s") 
+
+#Ocupan filas 6;7   
+txt_3_label = ctk.CTkLabel(buttons_frame, width=5, height=10, text="Aceleración: ", font=ctk.CTkFont(size=12, weight="bold"))
+txt_3_label.grid(row=6, column=0, columnspan=2, pady=15, sticky="sew")
+txt_3 = ctk.CTkTextbox(buttons_frame, width=60, height=8, activate_scrollbars=False)                              
+txt_3.grid(row=7, column=0, padx=2, pady=5, sticky="ne")
+b_3 = ctk.CTkSegmentedButton(buttons_frame,width=60, height=10 , values=["Km/h", " m/s "], command=selected)
+b_3.grid(row=7, column=1, padx=2, pady=8, sticky="nw")                                                             
+b_3.set("Km/h") 
+
+#Ocupan filas 9;10
+txt_4_label = ctk.CTkLabel(buttons_frame, width=5, height=15, text="Algún otra función:", font=ctk.CTkFont(size=12, weight="bold"))
+txt_4_label.grid(row=9, column=0, columnspan=2, pady=15, sticky="sew")
+txt_4 = ctk.CTkTextbox(buttons_frame, width=60, height=8, activate_scrollbars=False)
+txt_4.grid(row=10, column=0, padx=2, pady=5, sticky="e")
+b_4 = ctk.CTkSegmentedButton(buttons_frame,width=60, height=10 , values=[" h "," m "," s "], command=selected)
+b_4.grid(row=10, column=1, padx=2, pady=8, sticky="w")
+b_4.set(" m ")
+
+
+#   Loop principal de la Ventana    #
+main_window.mainloop()
