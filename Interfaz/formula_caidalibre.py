1b1c24d29eee812d3fd8d895edbc1b62d893a09a
Ignacio Ocares <133908441+ChileMationsXD@users.noreply.github.com>
2023-06-14 14:10:21 -0400

    calculos para el grafico

---
+++ Interfaz/formula_caidalibre.py
@@ -0,0 +1,31 @@
+import math
+import matplotlib.pyplot as plt
+
+def caida_libre(altura):
+    altura_lista = []
+    tiempo_lista = []
+    tiempo_final = math.sqrt(2*altura/9.8)
+    contador_decimal = 0
+    index = 0
+
+    while contador_decimal != tiempo_final:
+        tiempo_lista.append(round(contador_decimal,2))
+        posicion = altura - 4.9*tiempo_lista[index]**2
+        altura_lista.append(posicion)
+
+        if contador_decimal >= tiempo_final-0.01:
+            break
+
+        contador_decimal += 0.01
+        index += 1
+
+    # Fig es la "figura" resultante del grafico de ax
+    # ax es el unico grafico
+    fig, ax = plt.subplots()
+    ax.plot(tiempo_lista, altura_lista, label="Altura")
+    ax.set_xlabel("Tiempo (s)")
+    ax.set_ylabel("Altura (m)")
+    ax.set_title("Caída libre de un objeto")
+    ax.legend()
+
+    return fig
