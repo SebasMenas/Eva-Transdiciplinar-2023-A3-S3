7363b9c10c6c93e0c4b3356374e91d0b61eba7f3
LemonMenosQInexperto <sebastian.menafuentealba8@gmail.com>
2023-06-13 23:37:37 -0400

    Actu de Simubase
    base que se usara para la simulacion
    
    Co-Authored-By: Ignacio Ocares <133908441+ChileMationsXD@users.noreply.github.com>
    Co-Authored-By: Maximo Mora <96755454+StillEffect6@users.noreply.github.com>

---
+++ Simulacion/simulacionBase.py
@@ -0,0 +1,32 @@
+import tkinter 
+import time
+from tkinter import *
+
+tk = Tk()
+Res = [400,600] #Resolucion
+panta = Canvas(tk, width= Res[0], height= Res[1]) #pantalla principal (aun falta escalarla)
+panta.pack() # se llama a esta funcion para hacer los diferentes cambios
+grav,_suelo = (9.8),500,    #Gravedad(9,8 (m/s)^2), Nivel del suelo referente a la pantalla
+panta.create_line(0,500,400,500) #linea del suelo
+
+def obj_fall(PosInicial,tiempodereproduccion):
+    global grav, pY,_suelo  #llamar a globales
+    _y  = _suelo-PosInicial #donde empezara a caer (esto esta en Metros)
+    _V  = 0                 #velocidad
+    ball = panta.create_oval(70,10,70+40,10+40,fill= "RED") #dibujar pelota
+    _on = True
+    while _on:
+        panta.move(ball, 0, _V)            #para que la bola se mueva
+        _V += grav                         #aumentar la velocidad con la aceleracion
+        _y += _V                           #aumentar la posicion del objeto
+        print(_y)
+        if _y + _V >= _suelo:              #si la posicion con la velocidad llegan a superar la distancia del suelo
+            _y  = _suelo                   #dejar el objeto en el suelo
+            _V  = 0                        #la velocidad es 0 para
+            panta.moveto(ball, 70, _y-40)  #dibujar que cayo al suelo  
+            _on = False                    #terminar while
+        time.sleep(tiempodereproduccion)   #tiempo en que reproduce todo
+        tk.update()                        
+            
+obj_fall(600,0.0375)
+tk.mainloop()
