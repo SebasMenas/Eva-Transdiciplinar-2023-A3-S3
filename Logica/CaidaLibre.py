c71980ea157e32df1e6ee5bef9625c0615c51252
Maximo Mora <96755454+StillEffect6@users.noreply.github.com>
2023-06-13 22:22:27 -0400

    Update CaidaLibre.py
    mejora de la formula de caida libre

--- Logica/CaidaLibre.py
+++ Logica/CaidaLibre.py
@@ -1,44 +1,28 @@
 import math
-import  matplotlib.pyplot as plt
-altura = 1000
-tiempo  = 0
-Posicion = 1
-Velocidad = 0
-alturaLista = []
-tiempoLista = []
-TiempoFinal  = math.sqrt(2*altura/9.8)
-print(round(TiempoFinal,2))
-TiempoFinalEntero = int(TiempoFinal) + 1
-
-contadorDecimal = 0
-
-while contadorDecimal != TiempoFinal:
-    tiempoLista.append(contadorDecimal)
-    if contadorDecimal >= TiempoFinal-0.01:
-        break
-    contadorDecimal += 0.01
-
-IOK = True
-index = 0
-while IOK:
-    if index >= len(tiempoLista):
-        IOK = False
-    else:
-        AlturaFormula = altura - 4.9*tiempoLista[index]**2
-
-        alturaLista.append(AlturaFormula)
-        print(AlturaFormula)
-    index += 1
-
-
-print(tiempoLista)
-print(alturaLista)
-
-
-plt.xlabel("Tiempo")
-plt.ylabel("Altura")
-plt.plot(tiempoLista,alturaLista)
-plt.show()
 
 
 
+
+def caida_libre(altura):
+    altura_lista = []
+    tiempo_lista = []
+    tiempo_final = math.sqrt(2*altura/9.8)
+    contador_decimal = 0
+    index = 0
+    print(tiempo_final)
+    while contador_decimal != tiempo_final:
+        tiempo_lista.append(round(contador_decimal,2))
+        posicion = altura - 4.9*tiempo_lista[index]**2
+        altura_lista.append(posicion)
+
+        if contador_decimal >= tiempo_final:
+            break
+
+        contador_decimal += 0.01
+        index += 1
+
+
+    print(altura_lista)
+    print(tiempo_lista)
+
+caida_libre(25)
